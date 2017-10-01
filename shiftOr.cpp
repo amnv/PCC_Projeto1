fd346550870d70e0b327bd3719cbceb3f3c5a7d0
WisON27 <vrm@cin.ufpe.br>
2017-10-01 17:55:51 -0300

    Criando funcoes char_mask e shiftor, ainda com problemas.

--- shiftOr.cpp
+++ shiftOr.cpp
@@ -8,7 +8,7 @@ private:
 public:
     shiftOr(){} 
     ~shiftOr(){}
-    void action(string ppat, string stxt, bitset<T> & R){ //recebe 
+    void action(string ppat, string stxt, bitset<T> & R){ //recebe pedaco do padrao e pedaco do texto e coloca em R 0 ou 1 
         int patsize = ppat.size();
         int txtsize = stxt.size(); 
         if (ppat != stxt.substr(txtsize-patsize,txtsize)){
@@ -34,21 +34,53 @@ public:
 
 
 };
-// map<string, string> char_mask(string pat, string* ab){
-//     map <string, string> masks = NULL;
-//     int m = pat.size();
-//     masks[]
-// }
+map<char, bitset<T>> char_mask(string pat, string ab){
+    map <char, bitset<T>> masks;
+    bitset<T> posmask = bitset<T>().set();
+    posmask[0] = 0;
+    for (int i=0; i<ab.size();i++){ //colocando bitset para cada elemento do alfabeto
+        char e = pat[i];
+        masks[e] = bitset<T>().set();
+    }
+    for (int i=0; i<pat.size(); i++){
+        char e = pat[i];
+        masks[e] &= posmask;
+        posmask = posmask << 1;
+        posmask[0] = 1;
+    }
+    return masks;
+}
 
-// void shiftor(string txt, string pat, string* ab){
-//     int n = txt.size();
-//     int m = pat.size();
-//     deque<bool> a;
-// }
+deque<int> shiftor(string txt, string pat, string ab){
+    map<char, bitset<T>> masks = char_mask(pat, ab);
+    bitset <T> S;
+    S.set();
+    deque <int> occ;
+    for (int i=0; i<txt.size(); i++){
+        char e = txt[i];
+        S = S << 1 | masks[e];
+        if (S[txt.size()-1] == 0){
+            occ.push_back(i-pat.size());
+        }
+    }
+    return occ;
+}
+
+template <class P>
+string fprint(deque<P> a){
+    string r = "";
+    for(int i=0; i<a.size(); i++){
+        ostringstream oss;
+        oss << a[i];
+        r = r + oss.str() + " ";
+    }
+    return r;
+}
 
 int main(){
-    shiftOr* s = new shiftOr();
-    s->action2("abra", "cadabra");
+    string s = fprint(shiftor("abracadabra", "abra", "abcdr"));
+    cout << s << endl;
+
     /*
     deque<int> deck;
     cout << "\n" << deck[0] << "\n";
@@ -69,4 +101,24 @@ int main(){
 https://github.com/paguso/if76720172/commit/dd87fb4813623a1d1b415e115928cfe42a43afa9
 http://www.cin.ufpe.br/~paguso/courses/if767/bib/Baeza_1992.pdf
 http://www-igm.univ-mlv.fr/~lecroq/string/node6.html
+*/
+
+
+/**
+    funcao char_mask:
+    recebe o padrao e o alfabeto
+    masks eh um array de bitarray, tem 1 bitarray pra cada elemento do alfabeto
+    para cada elemento no alfabeto adiciona um indice bitarray no array masks com todos valores setados
+    cria-se um bitarray posmask com todos setados menos o ultimo
+    para cada elemento do padrao, faz-se que sua mascara eh sua mascara & a posmask, entao da-se um shift left na pos-mask e | 1 (coloca seu ultimo elemento como setado).
+    retorna o array de bitarrays, masks
+
+    funcao shift-of:
+    recebe o texto, o padrao e o alfabeto
+    cria-se o masks de todos caracteres do padrao
+    cria-se um bitarray S com tudo setado
+    para cada elemento do texto, faz-se S = S << 1 | masks[elemento], se bit mais significativo de S estah apagado, registra-se como ocorrencia a posicao do elemento - o tamanho do padrao (para o inicio da ocorrencia)
+
+    retorna as ocorrencias de match to padrao para aquele texto
+
 */
