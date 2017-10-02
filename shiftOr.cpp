5f329208d74fe57c36d1445bf96b0059930b5188
Jailson Dias <jcd2@cin.ufpe.br>
2017-10-02 15:05:36 -0300

    ShiftOr aparentemente funcional.

--- shiftOr.cpp
+++ shiftOr.cpp
@@ -8,7 +8,8 @@ private:
 public:
     shiftOr(){} 
     ~shiftOr(){}
-    void action(string ppat, string stxt, bitset<T> & R){ //recebe pedaco do padrao e pedaco do texto e coloca em R 0 ou 1 
+/*
+void action(string ppat, string stxt, bitset<T> & R){ //recebe pedaco do padrao e pedaco do texto e coloca em R 0 ou 1 
         int patsize = ppat.size();
         int txtsize = stxt.size(); 
         if (ppat != stxt.substr(txtsize-patsize,txtsize)){
@@ -31,24 +32,35 @@ public:
             std::cout << R << std::endl;
         }
     }
-
-
+*/
 };
 map<char, bitset<T>> char_mask(string pat, string ab){
-    map <char, bitset<T>> masks;
+    map <char, bitset<T>> masks; //as mascaras para cada letra do alfabeto
     bitset<T> posmask = bitset<T>().set();
-    posmask[0] = 0;
+    posmask.reset(0); //comecando com 111...0
+
     for (int i=0; i<ab.size();i++){ //colocando bitset para cada elemento do alfabeto
-        char e = pat[i];
-        masks[e] = bitset<T>().set();
+        char e = ab[i];
+        masks[e] = bitset<T>().set(); //comecando com 1
     }
-    for (int i=0; i<pat.size(); i++){
+    for (int i=0; i<pat.size(); i++){ 
         char e = pat[i];
         masks[e] &= posmask;
         posmask = posmask << 1;
-        posmask[0] = 1;
+        posmask[0] = 1; //ou 1
     }
     return masks;
+}
+
+void printDebug(deque<bitset<T>> deck, string pat, string txt){
+    cout << "  " << txt << endl;
+    for (int j=0; j<pat.size();j++){
+        cout << pat[j] << ' ';
+        for (int i=0; i<deck.size(); i++){
+            cout << deck[i][j];
+        }
+        cout << endl;
+    }
 }
 
 deque<int> shiftor(string txt, string pat, string ab){
@@ -56,13 +68,16 @@ deque<int> shiftor(string txt, string pat, string ab){
     bitset <T> S;
     S.set();
     deque <int> occ;
+    deque <bitset<T>> debug;
     for (int i=0; i<txt.size(); i++){
         char e = txt[i];
         S = S << 1 | masks[e];
         if (S[pat.size()-1] == 0){
             occ.push_back(i-pat.size()+1);
         }
+        debug.push_back(S);
     }
+    printDebug(debug, pat, txt);
     return occ;
 }
 
@@ -78,22 +93,9 @@ string fprint(deque<P> a){
 }
 
 int main(){
-    string s = fprint(shiftor("church", "ch", "chur"));
-    cout << s << endl;
-    
-    /*
-    deque<int> deck;
-    cout << "\n" << deck[0] << "\n";
-    string a = "opa";
-    map<string, string> dict;
-    dict.insert(pair<string, string>("a", "asdas"));
-    dict.insert(pair<string, string>("b", "1"));
-
-    cout << dict["a"] << endl;
-
-    for (map<string, string>::iterator it = dict.begin(); it != dict.end(); ++it)
-        cout << it->first << " => " << it->second << '\n';
-*/
+    string abc =  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    string s = fprint(shiftor("babracadabrac", "abra", abc));
+    cout << "\n" << s << endl;
     return 0;
 }
 
@@ -104,7 +106,7 @@ http://www-igm.univ-mlv.fr/~lecroq/string/node6.html
 */
 
 
-/**
+/** Algoritmos
     funcao char_mask:
     recebe o padrao e o alfabeto
     masks eh um array de bitarray, tem 1 bitarray pra cada elemento do alfabeto
