057670ca9778c3bb798a0332aa447d5b5b034e82
vrm <vrm@cin.ufpe.br>
2017-10-10 20:19:25 -0300

    Adicionando links de auxilio

--- wuManber.h
+++ wuManber.h
@@ -10,3 +10,9 @@ public:
 };
 
 #endif
+
+/* http://www.cin.ufpe.br/~paguso/courses/if767/bib/Wu_1992.pdf
+ * https://pt.slideshare.net/mailund/wu-mamber-string-algorithms-2007
+ * https://arxiv.org/pdf/1407.2889.pdf
+ * http://repositorio.ufpe.br/bitstream/handle/123456789/2533/arquivo4823_1.pdf
+*/
