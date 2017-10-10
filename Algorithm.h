105bd6250481447d2366e112dfadc721db45c678
vrm <vrm@cin.ufpe.br>
2017-10-10 19:21:42 -0300

    Adicionando classes fachadas

---
+++ Algorithm.h
@@ -0,0 +1,6 @@
+class Algorithm{
+public:
+    virtual int count(); //imprime a qtde total de ocorrencias
+    virtual deque<int> occ(); //lista as linhas das ocorrencias
+    
+}
---
+++ AlgorithmApproximate.h
@@ -0,0 +1,5 @@
+class AlgorithmApproximate : Algorithm{
+public:
+    virtual deque<int> dist(int e); //localiza as ocorrencias aproximadas do padrao a uma distancia de edicao maxima e
+    
+}
