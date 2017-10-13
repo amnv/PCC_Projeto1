2316181b174aa0c716c2e960e46e7ec83a415679
amnv <amnv@cin.ufpe.br>
2017-10-13 18:15:52 -0300

    add funcao

--- Algorithm.h
+++ Algorithm.h
@@ -1,4 +1,5 @@
 #include <deque>
+#include <string>
 
 #ifndef ALGORITHM 
 #define ALGORITHM
@@ -8,7 +9,8 @@ class Algorithm
 public:
     virtual int count() = 0; //imprime a qtde total de ocorrencias
     virtual std::deque<int> occ() = 0; //lista as linhas das ocorrencias
-    
+    virtual void setPat(std::string pat) = 0;
+    virtual void setText(std::string text) = 0;
 };
 
 #endif
